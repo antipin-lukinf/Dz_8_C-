530e616f4ad0ff7728e1924c8bfc12a3ca3c4594
Drei4 <antipin-lukinf@mail.ru>
2022-09-04 14:14:11 +0300

    add zadach54

---
+++ zadach54/Program.cs
@@ -0,0 +1,94 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+using System;
+int[,] GenerateArray(int t, int i)          //генерация массива
+{
+    int[,] table = new int[t, i];
+    Random random = new Random();
+    for (int a = 0; a < t; a++)
+    {
+        for (int b = 0; b < i; b++)
+        {
+            table[a, b] = random.Next(0, 9);
+        }
+    }
+    return table;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSort(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = 0; j < inArray.Length - i - 1; j++)
+        {
+            if (inArray[j] > inArray[j + 1])
+            {
+                int temp = inArray[j];
+                inArray[j] = inArray[j + 1];
+                inArray[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void Insert(bool isRow, int dim, int[] source, int[,] dest)
+{
+    for (int k = 0; k < source.Length; k++)
+    {
+        if (isRow)
+        {
+            dest[dim, k] = source[k];
+        }
+        else
+        {
+            dest[k, dim] = source[k];
+        }
+    }
+
+}
+
+
+
+
+
+
+
+int colCount = 6;
+int rowCount = 5;
+int[,] arr = GenerateArray(rowCount, colCount);
+Console.WriteLine("Исходный массив");
+PrintArray(arr);
+
+
+Console.WriteLine("Сортировка по столбцам");
+int[] col = new int[rowCount];          // отсортированный массив
+for (int i = 0; i < colCount; i++)
+{
+    for (int j = 0; j < rowCount; j++)
+    {
+        col[j] = arr[j,i];
+    }
+    BubbleSort(col);
+    Insert(false, i, col, arr);
+}
+PrintArray(arr);
